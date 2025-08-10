146ba40adbd663ed9c30301125fc80403e1e2f33
Felipe Moliari <felipemoliaribarbosa@gmail.com>
2025-08-10 20:58:21 -0300

    Jogo da cobra

--- js/utils/helpers.js
+++ js/utils/helpers.js
@@ -18,7 +18,7 @@ export function formatTime(milliseconds) {
     const seconds = Math.floor(milliseconds / 1000);
     const minutes = Math.floor(seconds / 60);
     const remainingSeconds = seconds % 60;
-    
+  
     return `${minutes.toString().padStart(2, '0')}:${remainingSeconds.toString().padStart(2, '0')}`;
 }
 
